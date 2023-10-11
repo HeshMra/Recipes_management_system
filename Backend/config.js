cdee422e02b2f82d0c7954b929e87993a3d3d3f1
heshith <heshith619@gmail.com>
2023-10-11 11:22:05 +0530

    App connected to the Database

--- Backend/config.js
+++ Backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555; //defining port number
+export const mongoDBURL =
+    'mongodb+srv://heshith619:L3omNgrA1r9rv8OY@merncluster.4ttktx4.mongodb.net/recipes?retryWrites=true&w=majority'
--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,27 @@
 import express from "express";
 const app = express();
-import {PORT} from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
-app.listen(PORT,()=>{
-    console.log(`App is listening to port ${PORT}`); //PORT
+
+
+app.get('/', (request, response) => { //get request
+    console.log(request);
+    return response.status(234).send('Welcome to Mern Stack Tutorial')
+});
+
+
+
+
+
+
+mongoose.connect(mongoDBURL)
+    .then(() => {
+        console.log('App Connected to Database');
+        app.listen(PORT, () => {
+            console.log(`App is listening to port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error(error); // Use console.error to log the error
     });
