bbcd1e80efc3fd1180ecc7d7918314d9162c29ed
heshith <heshith619@gmail.com>
2023-10-11 13:12:29 +0530

    suucessfully implemented CRUD

--- Backend/index.js
+++ Backend/index.js
@@ -1,20 +1,73 @@
-import express from "express";
-const app = express();
+import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import Recipe from "./models/RecipesModel.js";
+const app = express();
 
-
+//Middleware for parsing request
+app.use(express.json());
 
 app.get('/', (request, response) => { //get request
     console.log(request);
     return response.status(234).send('Welcome to Mern Stack Tutorial')
 });
 
+//Route for save a new recipe
+app.post('/recipes', async (request, response) => {
+    try {
+        if (
+            !request.body.name ||
+            !request.body.description ||
+            !request.body.ingrediants ||
+            !request.body.category
+        ) {
+            return response.status(400).send({
+                message: 'Send all required Fields: name, description,ingrediants,category',
+            });
+        }
+        const newRecipe = {
+            name: request.body.name,
+            description: request.body.description,
+            ingrediants: request.body.ingrediants,
+            category: request.body.category,
+        };
+        const recipe = await Recipe.create(newRecipe);
+        return response.status(201).send(recipe);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+//Route for GET all recipes from database
+app.get('/recipes', async (request, response) => {
+    try {
+        const recipe = await Recipe.find({});
+        return response.status(200).json({
+            count: recipe.length,
+            data: recipe
+        });
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+})
+
+//Route for GET One recipe from database by ID
+app.get('/recipes/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+        const recipe = await Recipe.findById(id);
+        return response.status(200).json(recipe);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
 
 
 
-
-
+//DB Connection
 mongoose.connect(mongoDBURL)
     .then(() => {
         console.log('App Connected to Database');
---
+++ Backend/models/RecipesModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+const recipeSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        ingrediants: {
+            type: String,
+            required: true,
+        },
+        category: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+const Recipe = mongoose.model('Recipe', recipeSchema); // Use 'Recipe' as the model name
+export default Recipe; // Export the Recipe model
